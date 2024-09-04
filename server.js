fc1e42d301ffa6318014863565e601510abef7fa
Michael Ptak <michael.p11@proton.me>
2024-09-03 20:22:09 -0400

    Environment variables

--- server.js
+++ server.js
@@ -12,11 +12,13 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-      host : '127.0.0.1',
-      user : 'postgres',
+      connectionString: process.env.DATABASE_URL,
+      ssl: { rejectUnauthorized: false},
+      host : process.env.DATABASE_HOST,
+      user : process.env.DATABASE_USER,
       port: 5432,
-      password : '',
-      database : 'smart-brain'
+      password : process.env.DATABASE_PW,
+      database : process.env.DATABASE_DB
     }
 });
 
